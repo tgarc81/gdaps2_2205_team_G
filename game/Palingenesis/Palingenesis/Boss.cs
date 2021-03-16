ce85b78ef3d7679a89d4cdfcfb80a1573b93d740
Lucas Diamond <lgd1649@rit.edu>
2021-03-16 13:16:03 -0400

    changed

--- game/Palingenesis/Palingenesis/Boss.cs
+++ game/Palingenesis/Palingenesis/Boss.cs
@@ -7,17 +7,33 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Palingenesis
 {
+    public enum bossName
+    {
+        RiceGoddess,
+
+    }
     class Boss : Character
     {
+        private bossName type;
 
-        public Boss (int health, int moveSpeed, int attackSpeed, int Damage, Texture2D texture, Rectangle position, int windowWidth, int windowHeight) : base(health, moveSpeed, attackSpeed, Damage, texture, position, windowWidth, windowHeight)
+        public Boss (int health, int moveSpeed, int attackSpeed, int Damage, Texture2D texture, Rectangle position, int windowWidth, int windowHeight, bossName type) : base(health, moveSpeed, attackSpeed, Damage, texture, position, windowWidth, windowHeight)
         {
-
+            this.type = type;
         }
 
         public override void Update()
         {
             //does nothing for now
+        }
+
+        //standard attacks
+
+        public void specialAttack()
+        {
+            if(type = bossName.RiceGoddess)
+            {
+
+            }
         }
     }
 }
--- game/Palingenesis/Palingenesis/Game1.cs
+++ game/Palingenesis/Palingenesis/Game1.cs
@@ -68,9 +68,9 @@ namespace Palingenesis
 
             playerAsset = Content.Load<Texture2D>("playerPlaceHolderTexture");
             attackTexture = Content.Load<Texture2D>("attackPlaceholder");
-            player = new Player(100, 10, 10, 20, playerAsset, new Rectangle(200, 200, 50, 50), windowHieght, windowWidth);
+            player = new Player(100, 10, 10, 20, playerAsset, new Rectangle(200, 200, 50, 50), windowHeight, windowWidth);
             //note: make a placeholder asset for the boss
-            boss1= new Boss(1000, 0, 10, 10, playerAsset, new Rectangle(500, 500, 10, 10), windowWidth, windowHeight);
+            boss1= new Boss(1000, 0, 10, 10, playerAsset, new Rectangle(500, 500, 10, 10), windowWidth, windowHeight, bossName.RiceGoddess);
                 
         }
 
@@ -147,7 +147,7 @@ namespace Palingenesis
                 case gameState.Pause:
 
                     //pressing escape on pause restarts the game
-                    if(SingleKeyPress(Keys.Escape, kbState))
+                    if(SingleKeyPress(Keys.P, kbState))
                     {
                         currentState = gameState.Game;
                     }
