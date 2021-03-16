6d5eff84bfe8470c50a487e7840122353d2966f2
Lucas Diamond <lgd1649@rit.edu>
2021-03-16 14:07:05 -0400

    got 1 attack method mostly down

--- game/Palingenesis/Palingenesis/Boss.cs
+++ game/Palingenesis/Palingenesis/Boss.cs
@@ -17,10 +17,12 @@ namespace Palingenesis
         private bossName type;
         private Point playerPosition;
         private List<Bullet> projectileList = new List<Bullet>();
-
-        public Boss (int health, int moveSpeed, int attackSpeed, int Damage, Texture2D texture, Rectangle position, int windowWidth, int windowHeight, bossName type) : base(health, moveSpeed, attackSpeed, Damage, texture, position, windowWidth, windowHeight)
+        private Random rng = new Random();
+        private Texture2D bulletTexture;
+        public Boss (int health, int moveSpeed, int attackSpeed, int Damage, Texture2D texture, Rectangle position, int windowWidth, int windowHeight, bossName type, Texture2D bulletTexture) : base(health, moveSpeed, attackSpeed, Damage, texture, position, windowWidth, windowHeight)
         {
             this.type = type;
+            this.bulletTexture = bulletTexture;
         }
 
         public override void Update()
@@ -28,9 +30,36 @@ namespace Palingenesis
             //does nothing for now
         }
 
-        //standard attacks
-        public void Line()
+        //helper method fills bullet list
+        public void CreateProjectiles(int amount, direction direction, Rectangle position, int xSpacing, int ySpacing)
         {
+            for(int i = 0; i < amount; i++)
+            {
+               
+                projectileList.Add(new Bullet(bulletTexture, position, this.windowHeight, this.windowWidth));
+                position.X += xSpacing;
+                position.Y += ySpacing;
+
+            }
+        }
+
+        //used to decide what attack the boss will use with a random
+        public void AI()
+        {
+
+        }
+
+        //standard attacks
+        public void Line(Player target)
+        {
+
+            //if the player is to the left of the boss
+            if(target.Position.X < this.position.X)
+            {
+                //creates between 1 and 5 projectiles that fly to the left
+                //first bullet is created at Y = 100 and each bullet after that is 25 pixels further down the screen
+                CreateProjectiles(rng.Next(1, 6), direction.left, new Rectangle((this.Position.X - 50), 100, 25, 25), 0, 25);
+            }
 
         }
         public void Circle()
--- game/Palingenesis/Palingenesis/bullet.cs
+++ game/Palingenesis/Palingenesis/bullet.cs
@@ -7,19 +7,52 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Palingenesis
 {
+    public enum direction
+    {
+        //at the moment they can only move in straight lines
+        up,
+        down,
+        left,
+        right,
+    }
     //projectiles to be fired by bosses
     class Bullet : Character
     {
+        //use 1,2,3,4 for up down left right
+        private direction direction;
+        private Texture2D texture;
+
+        public direction Direction
+        {
+            get { return this.direction; }
+            set { direction = value; }
+        }
 
         //contructor
-        public Bullet(int moveSpeed, int Damage, Texture2D texture, Rectangle position, int windowHeight, int windowWidth) : base(0, moveSpeed, 0, Damage, texture, position, windowHeight, windowWidth)
+        public Bullet(Texture2D texture, Rectangle position, int windowHeight, int windowWidth) : base(0, 10, 0, 10, texture, position, windowHeight, windowWidth)
         {
 
         }
 
         public override void Update()
         {
-            //TODO, figure out what needs to be updated 
+            if(Direction == direction.up)
+            {
+                //moves up the screen by the set amount movespeed
+                position.Y -= moveSpeed;
+            }
+            else if(Direction == direction.down)
+            {
+                position.Y += moveSpeed;
+            }
+            else if (Direction == direction.left)
+            {
+                position.X -= moveSpeed;
+            }
+            else if (Direction == direction.right)
+            {
+                position.X += moveSpeed;
+            }
         }
     }
 }
