01a748e4ffd8781cb4aff3fb43482392e2236a35
Aedan Frank <acf6919@g.rit.edu>
2021-03-28 21:50:32 -0400

    rice goddess implemented

--- game/Palingenesis/Palingenesis/Game1.cs
+++ game/Palingenesis/Palingenesis/Game1.cs
@@ -132,7 +132,7 @@ namespace Palingenesis
             textboxNameVN = Content.Load<Texture2D>("rustyName");
             backgroundVN = Content.Load<Texture2D>("bayo2");
             playerVN = Content.Load<Texture2D>("theo");
-            bossVN = Content.Load<Texture2D>("bingus");
+            bossVN = Content.Load<Texture2D>("ricegoddess");
             forwardVN= Content.Load<Song>("forward_sound");
 
 
